203b257ad3b799d245ef073f4e882b40ce14bca5
almagyovai <167795724+almagyovai@users.noreply.github.com>
2024-09-04 22:43:03 +0300

    Organized the containers

---
+++ Containers/LeftMenuContainer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function LeftMenuContainer() {
+  return <div></div>
+}
+
+export {LeftMenuContainer};
---
+++ Containers/TopbarContainer.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function TopbarContainer() {
+  return <div></div>
+}
+
+export {TopbarContainer};
