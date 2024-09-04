2551e670bf4be44902c2aeea9462f9088c1efc70
almagyovai <167795724+almagyovai@users.noreply.github.com>
2024-09-04 22:43:19 +0300

    Delete Components directory

--- Components/MainContainer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function MainContainer() {
-  return <div></div>;
-}
-
-export {MainContainer};
--- Components/RightMenu.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function RightMenu() {
-    return <div></div>;
-  }
-  
-  export {RightMenu};
