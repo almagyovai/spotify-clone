36b30b58bc1bacac390a2dfbd6f9d88b655c324d
almagyovai <167795724+almagyovai@users.noreply.github.com>
2024-09-08 18:41:48 +0300

    Assigned a unique path to each route

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./Assets/App.css";
 
 import HomePage from "./Pages/HomePage.js";
+import NotificationPage from "./Pages/NotificationPage.js";
 import NotFound from "./Pages/NotFound.js";
 
 
@@ -10,6 +11,7 @@ function App() {
   <BrowserRouter>
   <Routes>
     <Route path="/" element={<HomePage />} />
+    <Route path="/NotificationPage" element={<NotificationPage />} />
     <Route path="/*" element={<NotFound />} />
   </Routes>
   </BrowserRouter>
