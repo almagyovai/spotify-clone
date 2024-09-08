7d0001836b77c6b4ea1f6ec031546e8a6fbddfd1
almagyovai <167795724+almagyovai@users.noreply.github.com>
2024-09-08 23:16:03 +0300

    Made a MenuList map

--- LeftMenuList.js
+++ LeftMenuList.js
@@ -1,21 +1,47 @@
 import React from 'react';
-import { IoAlbums } from "react-icons/io5";
-import { IoRadio } from "react-icons/io5";
+import { IoAlbums, IoRadio } from "react-icons/io5";
 import { BiPulse } from "react-icons/bi";
-import { FaPodcast } from "react-icons/fa";
-import { FaMicrophone } from "react-icons/fa";
-
+import { FaPodcast, FaMicrophone } from "react-icons/fa";
 
 function LeftMenuList() {
-  return (
-    <div>
-      <IoAlbums />
-      <IoRadio />
-      <BiPulse />
-      <FaPodcast />
-      <FaMicrophone />
-    </div>
-  )
+    const MenuList = [
+        {
+            id: 1,
+            icon: <BiPulse />,
+            name: "Discover",
+        },
+        {
+            id: 2,
+            icon: <IoRadio />,
+            name: "Radio",
+        },
+        {
+            id: 3,
+            icon: <FaMicrophone />,
+            name: "Artists",
+        },
+        {
+            id: 4,
+            icon: <IoAlbums />,
+            name: "Albums",
+        },
+        {
+            id: 5,  // Fixed duplicate id
+            icon: <FaPodcast />,
+            name: "Podcasts",
+        },
+    ];
+
+    return (
+        <div>
+            {MenuList.map(item => (
+                <div key={item.id} style={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+                    <div style={{ marginRight: '10px' }}>{item.icon}</div>
+                    <span>{item.name}</span>
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default LeftMenuList;
