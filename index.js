9515bc49499bd322f37a08e03ff1f042fb9e097d
almagyovai <167795724+almagyovai@users.noreply.github.com>
2024-09-08 18:33:19 +0300

    corrected rendering

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import HomePage from "./Pages/HomePage.js";
-import NotFound from "./Pages/NotFound.js"
-import "./Assets/App.css";
-import "react-toastify/dist/ReactToastify.css";
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <HomePage />
-      <NotFound />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <App />
+  </React.StrictMode>
 );
+
