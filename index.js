c024cad232f1e81c0cc8836e7b3e0741ccf39383
almagyovai <167795724+almagyovai@users.noreply.github.com>
2024-09-08 17:55:47 +0300

    added Notfound Page to index.js

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import App from "./App";
-import "./style.css";
+import HomePage from "./Pages/HomePage.js";
+import NotFound from "./Pages/NotFound.js"
+import "./Assets/App.css";
 import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
-      <ToastContainer
-       position="bottom-center"
-       autoClose={2000}
-       hideProgressBar={true}
-       closeButton={false}
-       theme="colored"
-       icon={false} />
+      <HomePage />
+      <NotFound />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
