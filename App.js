858f9d0f9c825203b864c0279ad5113aec62be68
almagyovai <167795724+almagyovai@users.noreply.github.com>
2024-09-15 19:17:50 +0300

    Passed the token as a prop to the pages within the router

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./Assets/App.css";
-
 import HomePage from "./Pages/HomePage.js";
 import NotificationPage from "./Pages/NotificationPage.js";
+import SearchPage from "./Pages/SearchPage.js";
 import NotFound from "./Pages/NotFound.js";
+import React, { useState, useEffect } from 'react';
+import { loginToSpotify, getTokenFromUrl } from './spotifyAuth';
+import SpotifyDataComponent from './Components/CardList.js';
 
 
 function App() {
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    const tokenFromUrl = getTokenFromUrl();
+    if (tokenFromUrl) {
+      setToken(tokenFromUrl);
+    } else {
+      loginToSpotify();
+    }
+  }, []);
+
+  if (!token) {
+    return <div>Loading...</div>;
+  }
+
   return (
-  <BrowserRouter>
-  <Routes>
-    <Route path="/HomePage" element={<HomePage />} />
-    <Route path="/NotificationPage" element={<NotificationPage />} />
-    <Route path="/*" element={<NotFound />} />
-  </Routes>
-  </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/spotify-data" element={<SpotifyDataComponent token={token} />} />
+        <Route path="/" element={<HomePage token={token} />} />
+        <Route path="/NotificationPage" element={<NotificationPage token={token} />} />
+        <Route path="/SearchPage" element={<SearchPage token={token} />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
